16071c007843edf8ca190ecf72dec212b9d3b638
DanielCarrascoPallaleo <carrascopallaleo.daniel@gmail.com>
2025-11-06 20:51:10 -0400

    feat(configuracion): configuracion de dependencias instaladas main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// 1. Importa el tema de PrimeReact
+import 'primereact/resources/themes/saga-blue/theme.css'; 
+// 2. Importa estilos base de PrimeReact
+import 'primereact/resources/primereact.min.css';
+// 3. Importa los íconos
+import 'primeicons/primeicons.css';
+// 4. Importa Bootstrap (solo para la grilla)
+import 'bootstrap/dist/css/bootstrap-grid.min.css'; // Usamos solo la grilla
+
+import './index.css' // CSS personalizado
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
